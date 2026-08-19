cca77de2ec86c6e2952bcd7b38442f2cdfaf3d88
PedroPalandi <pedrohenriquepalandi@gmail.com>
2026-08-19 16:51:24 -0300

    Finalizações do site de tecnologia

---
+++ Projeto_NovaTecnologia/js/menu.js
@@ -0,0 +1,8 @@
+//Script para ativar o menu hambúrguer
+
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
